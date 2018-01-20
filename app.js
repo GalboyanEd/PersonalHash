cca91ff4ecd300cb05cdde34cfc52e71f80995ce
Galboyan Eduard <galoboed@gmail.com>
2018-01-20 16:58:34 +0400

    Added Please wait dialog

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var hash = require('object-hash');
+var readline = require('readline');
 var fullName = 'Eduard Galboyan';
+
+process.stdout.write('Please wait...');
 
 for(var i = 0;; i++){
 	var toHash = fullName + i;
 	var hashedName = hash(toHash);
 
-	if(startZero(hashedName) >= 10){
+	if(startZero(hashedName) >= 5){
+  		readline.cursorTo(process.stdout, 0);
+  		process.stdout.write('            ');
+  		readline.cursorTo(process.stdout, 0);
+  		
 		console.log('Your name is: ' + fullName);
 		console.log('The hash of: ' + toHash + ' is ' + hashedName);
 		console.log('Look it is starting with ' + startZero(hashedName) + ' zeros!');
 		break;
 	}
-
 }
 
 
